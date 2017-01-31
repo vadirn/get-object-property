44b7321cc642722eab30b22cb9c4277f65dd7449
Vadim Kotov <igeeko@gmail.com>
2017-01-31 12:28:06 +0600

    explicitly provide log function

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,4 @@
 import getObjectProperty from '../index';
-import { setLogFunction } from '../index';
 
 describe('getObjectProperty', () => {
   const logFn = jest.fn();
@@ -8,26 +7,25 @@ describe('getObjectProperty', () => {
     let result = getObjectProperty(() => foo.bar.foo, 'default');
     expect(result).toEqual('default');
     expect(logFn.mock.calls.length).toBe(0);
-    setLogFunction(logFn);
-    result = getObjectProperty(() => foo.bar.foo, 'default');
+    result = getObjectProperty(() => foo.bar.foo, 'default', logFn);
     expect(result).toEqual('default');
     expect(logFn.mock.calls.length).toBe(1);
   });
   it('returns default value if property is undefined', () => {
     const foo = {};
-    const result = getObjectProperty(() => foo.bar, 'default');
+    const result = getObjectProperty(() => foo.bar, 'default', logFn);
     expect(result).toEqual('default');
     expect(logFn.mock.calls.length).toBe(1);
   });
   it('logs error if property is undefined', () => {
     const foo = {};
-    const result = getObjectProperty(() => foo.bar.foo, 'default');
+    const result = getObjectProperty(() => foo.bar.foo, 'default', logFn);
     expect(result).toEqual('default');
     expect(logFn.mock.calls.length).toBe(2);
   });
   it('returns value if property exists', () => {
     const foo = { bar: 'foo' };
-    const result = getObjectProperty(() => foo.bar, 'default');
+    const result = getObjectProperty(() => foo.bar, 'default', logFn);
     expect(result).toEqual('foo');
   });
 });
